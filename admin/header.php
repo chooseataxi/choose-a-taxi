8212b03f738615767521ebc67cf54f3331c21503
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-03-27 15:43:25 +0530

    updated

--- admin/header.php
+++ admin/header.php
@@ -357,6 +357,19 @@ $active_page = $active_pageInfo['active_page'] ?? null;
                 padding-left: 20px !important;
                 justify-content: flex-start !important;
             }
+        @keyframes blink {
+            0% { opacity: 1; }
+            50% { opacity: 0.3; }
+            100% { opacity: 1; }
+        }
+
+        .blinking-text {
+            animation: blink 2s infinite;
+            color: var(--primary-green) !important;
+            font-weight: 700;
+            font-size: 1rem;
+            letter-spacing: 0.5px;
+            text-transform: uppercase;
         }
     </style>
 </head>
@@ -367,37 +380,54 @@ $active_page = $active_pageInfo['active_page'] ?? null;
         <!-- Wrapper started -->
 
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <div class="nav-link">
-                        <i class="fas fa-th-large"></i>
-                    </div>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="./" class="nav-link">Home</a>
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
             </ul>
-            <form class="form-inline ml-3">
-                <div class="input-group input-group-sm">
-                    <input class="form-control form-control-navbar" type="search" placeholder="Search" name="search">
-                    <div class="input-group-append">
-                        <button class="btn btn-navbar" type="submit">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-            </form>
+
+            <!-- Blinking Welcome Message -->
+            <div class="navbar-nav mx-auto">
+                <span class="blinking-text">Welcome to Choose a Taxi Admin Dashboard</span>
+            </div>
+
+            <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
+                <!-- Quick Actions Dropdown -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link" href="#messages">
-                        <i class="far fa-comments"></i>
-                        <span class="badge badge-danger navbar-badge">2</span>
+                    <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fas fa-bolt mr-1"></i> Quick Actions
                     </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <span class="dropdown-item dropdown-header">Administrative Tasks</span>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-car mr-2"></i> New Booking
+                        </a>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-user-plus mr-2"></i> Add Driver
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item dropdown-footer">See all actions</a>
+                    </div>
                 </li>
-                <li class="nav-item dropdown"><a class="nav-link" href="#notifications">
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">5</span>
+
+                <!-- Profile Dropdown -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <img src="./src/images/user-avtar.png" class="img-circle elevation-1 mr-2" style="width: 25px; height: 25px; object-fit: cover;" alt="User">
+                        <span>Admin</span>
                     </a>
+                    <div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
+                        <a href="profile.php" class="dropdown-item">
+                            <i class="fas fa-user-circle mr-2"></i> My Profile
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="logout.php" class="dropdown-item text-danger">
+                            <i class="fas fa-sign-out-alt mr-2"></i> Logout
+                        </a>
+                    </div>
                 </li>
             </ul>
         </nav>
