376e71b61bfb4826e9934ddacfa9335bf7f8a1f1
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-06-06 01:43:03 +0530

    updated

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 <!-- Custom Footer CSS -->
 <link rel="stylesheet" href="assets/css/footer.css">
 <!-- Custom Stats CSS -->
-<link rel="stylesheet" href="assets/css/stats.css">
+<!-- <link rel="stylesheet" href="assets/css/stats.css"> -->
 <!-- Custom Price List CSS -->
-<link rel="stylesheet" href="assets/css/price-list.css">
+<!-- <link rel="stylesheet" href="assets/css/price-list.css"> -->
 <!-- Custom Why Choose Us CSS -->
-<link rel="stylesheet" href="assets/css/why-choose-us.css">
+<!-- <link rel="stylesheet" href="assets/css/why-choose-us.css"> -->
 <!-- Custom App Download CSS -->
 <link rel="stylesheet" href="assets/css/app-download.css">
 <!-- Custom CTA Banner CSS -->
@@ -19,15 +19,11 @@
 <!-- Custom Fleet CSS -->
 <link rel="stylesheet" href="assets/css/fleet.css">
 <!-- Custom Testimonials CSS -->
-<link rel="stylesheet" href="assets/css/testimonials.css">
+<!-- <link rel="stylesheet" href="assets/css/testimonials.css"> -->
 
 <?php include 'components/hero.php'; ?>
 <?php include 'components/services.php'; ?>
-<?php include 'components/price-list.php'; ?>
 <?php include 'components/fleet.php'; ?>
-<?php include 'components/stats.php'; ?>
-<?php include 'components/testimonials.php'; ?>
-<?php include 'components/why-choose-us.php'; ?>
 <?php include 'components/cta-banner.php'; ?>
 <?php include 'components/app-download.php'; ?>
 
